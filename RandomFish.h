355dabef7f480a3bca76dc8b97caa6a963aeab2a
Shani Halali - שני הללי <shanihalaliafeka@gmail.com>
2024-08-28 13:45:25 +0300

    Update RandomFish.h- To linked list with VOID* of adding date and time

--- RandomFish.h
+++ RandomFish.h
@@ -1,38 +1,39 @@
+#ifndef RANDOMFISH_H
+#define RANDOMFISH_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#pragma warning (disable:4996)
-#define _CRT_SECURE_NO_WARNINGS
+#include <time.h>
 
 #define MAX_ITEMS 20
-#define OLDESTFISH(Fish){\
-				printf("the oldest fish : "); \
-				printf("name : %s ,", Fish->name); \
-				printf("age : %d \n", Fish->age); \
-}
+
 
 typedef enum {
-	ePoints, eStripes, eSlots, eNofrandomFishPattern
+    ePoints, eStripes, eSlots, eNofrandomFishPattern
 } eRandomFishPattern;
 
-
-static const char* randomFishPattern[eNofrandomFishPattern]
-= { "Points", "Stripes", "Slots" };
+static const char* randomFishPattern[eNofrandomFishPattern] = { "Points", "Stripes", "Slots" };
 
 
 typedef enum {
-	eBlack, eRed, ePurple, eGreen, eBlue, eYellow, eNofrandomFishColor
+    eBlack, eRed, ePurple, eGreen, eBlue, eYellow, eNofrandomFishColor
 } eRandomFishColor;
 
+static const char* randomFishColor[eNofrandomFishColor] = { "Black", "Red", "Purple", "Green", "Blue", "Yellow" };
 
-static const char* randomFishColor[eNofrandomFishColor] = { "Black", "Red", "Purple","Green", "Blue", "Yellow" };
+// Structure to hold timestamp information
+typedef struct {
+    time_t timestamp; // Timestamp of when the fish was added
+} FishTimestamp;
 
 
-typedef struct RandomFish
-{
-	int age;
-	enum eRandomFishPattern pattern;
-	enum eRandomFishColor color;
+typedef struct RandomFish {
+    int age;
+    eRandomFishPattern pattern;
+    eRandomFishColor color;
+    void* extraInfo; 
+    struct RandomFish* next; 
 } RandomFish;
 
 
@@ -42,3 +43,10 @@ void writeRandomFish(RandomFish* random, FILE* file);
 RandomFish* readRandomFishFromFile(FILE* fp);
 void writeRandomFishToBinaryFile(RandomFish* random, FILE* file);
 RandomFish* readRandomFishFromBinaryFile(FILE* file);
+void freeRandomFish(RandomFish* fish);
+void printFishTimestamp(RandomFish* fish);
+void addFishToList(RandomFish** head, RandomFish* newFish);
+void printFishList(RandomFish* head);
+void freeFishList(RandomFish* head);
+
+#endif // RANDOMFISH_H
