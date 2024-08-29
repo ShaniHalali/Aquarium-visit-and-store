5076cae3a5cef909da26a4584af1edb4e7435f73
Shani Halali - שני הללי <shanihalaliafeka@gmail.com>
2024-08-29 17:48:07 +0300

    w to file

--- generalFunctions.h
+++ generalFunctions.h
@@ -1,9 +1,13 @@
 #ifndef GENERAL_FUNCTION_H
 #define GENERAL_FUNCTION_H
 #include <stdio.h>
+#pragma warning (disable:4996)
+#include <stdlib.h>
 
 void printArr(const void** arr, int size, void (*print)(const void*));
 void clearInputBuffer();
+int getValidAge();
+int getValidLifeSpan(int age);
 void writeDoubleToBinaryFile(double value, FILE* file);
 double readDoubleFromBinaryFile(FILE* file);
 #endif 
